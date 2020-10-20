d486c857ae5f5516a6af4067c2e5f31208a81d9f
jkznst <2423541783@qq.com>
2020-10-20 14:46:03 +0800

    plot speed

--- projects/SixDPose/tools/plot_speed.py
+++ projects/SixDPose/tools/plot_speed.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_speed():
+def plot_speed_crpnet():
     plt.axvspan(25, 75, facecolor='#2ca02c', alpha=0.3)
 
     plt.scatter(2, 50.2, s=100, label='[82] w/ ref.')
@@ -19,6 +19,42 @@ def plot_speed():
     x = [30, 35, 43]
     y = [74.7, 73.6, 70.3]
     plt.plot(x, y, c='purple')
+
+    plt.xlabel('FPS')
+    plt.ylabel('ADD@0.1(%)')
+    plt.xlim(0, 75)
+    # plt.legend(['[82]', 'BB8[87]', 'SSD-6D[84]', 'YOLO6D[88]', 'CRPNet(ours)'])
+    plt.legend()
+    plt.show()
+
+def plot_speed_hcrnet():
+    plt.axvspan(25, 75, facecolor='#2ca02c', alpha=0.3)
+
+    plt.scatter(10, 79, s=100, marker='s', label='SSD-6D[84] w/ ref.')
+    plt.scatter(6, 62.7, s=100, marker='p', c='black', label='PoseCNN[86]')
+    # plt.scatter(3, 62.7, s=100, marker='^', label='BB8[87] w/ ref.')
+    plt.scatter(50, 56, s=100, marker='*', label='YOLO6D[88]')
+    plt.scatter(25, 86.2, s=100, label='PVNet[93]')
+    # plt.scatter(4.5, 64.7, s=100, marker='v', label='AAE[94] w/ ref.')
+    plt.scatter(6.5, 72.4, s=100, marker='X', c='brown', label='Pix2Pose[96]')
+    plt.scatter(18, 89.8, s=100, marker='>', c='#f5bf03', label='CDPN[97]')
+    plt.scatter(33, 83.0, s=100, marker='<', c='cyan', label='DPOD[98]')
+
+    # CRPNet
+    plt.scatter(30, 74.7, s=100, c='purple', marker='D', label='CRPNet(ours)')
+    plt.scatter(35, 73.6, s=100, c='purple', marker='D')
+    plt.scatter(43, 70.3, s=100, c='purple', marker='D')
+    x = [30, 35, 43]
+    y = [74.7, 73.6, 70.3]
+    plt.plot(x, y, c='purple')
+
+    # HCRNet
+    plt.scatter(21, 86.5, s=100, c='red', marker='P', label='HCRNet(ours)')
+    plt.scatter(25, 84.9, s=100, c='red', marker='P')
+    plt.scatter(32, 81.0, s=100, c='red', marker='P')
+    x = [21, 25, 32]
+    y = [86.5, 84.9, 81.0]
+    plt.plot(x, y, c='red')
 
     plt.xlabel('FPS')
     plt.ylabel('ADD@0.1(%)')
@@ -45,4 +81,4 @@ def plot_indirect_strategy():
     plt.show()
 
 if __name__ == "__main__":
-    plot_speed()
+    plot_speed_hcrnet()
